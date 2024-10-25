c28b11533c65866cf0ef2c2c2c220f40b370fab3
Dogmeat2000 <86206683+Dogmeat2000@users.noreply.github.com>
2024-10-25 12:20:09 +0200

    Added ClientSide entities

---
+++ Client/ClientApp/Entities/Forum.cs
@@ -0,0 +1,21 @@
+﻿namespace ClientApp.Entities;
+
+public class Forum {
+    public int Forum_id { get; set; }
+    public string Title_txt { get; set; }
+    public DateTime Timestamp_created { get; set; }
+    public DateTime Timestamp_modified { get; set; }
+    public DateTime Timestamp_deleted { get; set; }
+    public int LastPost_id { get; set; }
+    public int LastCommentPost_id { get; set; }
+    public int LastComment_id { get; set; }
+    public int Author_id { get; set; }
+    public int ParentForum_id { get; set; }
+
+    public Forum() {
+        Title_txt = "";
+        LastPost_id = -1;
+        LastCommentPost_id = -1;
+        LastComment_id = -1;
+    }
+}
---
+++ Client/ClientApp/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ClientApp.Entities;
+
+public class User {
+    public int User_id { get; set; }
+
+    public User(int user_id) {
+        User_id = user_id;
+    }
+    
+    public User() {
+        User_id = -1;
+    }
+}
