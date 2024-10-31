fdb3436897e066a30fe3fc741c3eceb25591c69e
Dogmeat2000 <86206683+Dogmeat2000@users.noreply.github.com>
2024-10-31 21:48:30 +0100

    Assignment 5: Completed (Mandatory Tasks)

--- Client/ClientApp/obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
+++ Client/ClientApp/obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0438d51dc78274093c7968990caa21b6df8ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37487e281db1f71815e79b1ef0d258ed1e20b99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ Server/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b688d51c4f191f0206d4e64f37727fc6071851b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37487e281db1f71815e79b1ef0d258ed1e20b99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
